8b356d3bd17a54d14e5f1a525782b75fb5ef4e7b
Janet <Janet@Janets-MacBook-Air.local>
2018-08-30 16:17:53 -0700

    added nav bar

---
+++ js/portfolio.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	// add any functionality and listeners you want here
+	console.log("Javascript is connected!");
+	$(".img-thumbnail").click(projectClick);
+}
+
+function projectClick(e){
+	e.preventDefault();
+	$(this).css("background-color", "#20415f")
+	$(this).parent().next().toggle();
+	console.log("It worked!")
+}
