ebb989e5350a274977b96dba5bdfe509fb75494e
Janet <Janet@Janets-MacBook-Air.local>
2018-08-31 11:08:19 -0700

    changed images and website text

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function()) {
 	initializePage();
-})
+}
 
 /*
  * Function that is called when the document is ready.
@@ -11,7 +11,6 @@ $(document).ready(function() {
 function initializePage() {
 	// add any functionality and listeners you want here
 	console.log("Javascript is connected!");
-<<<<<<< HEAD
 	$(".img-thumbnail").click(projectClick);
 }
 
@@ -19,7 +18,6 @@ function projectClick(e){
 	e.preventDefault();
 	$(this).css("background-color", "#20415f")
 	$(this).parent().next().toggle();
-=======
 }
 
 $(".img-thumbnail").click(projectClick);
@@ -27,6 +25,5 @@ $(".img-thumbnail").click(projectClick);
 function projectClick(e){
 	e.preventDefault();
 	$(this).css("background-color", "#000000")
->>>>>>> c9e8e79803c73ac6d4d88c5e2eb4f0c417ba3155
 	console.log("It worked!")
 }
